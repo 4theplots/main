0ad2c3ac1a94c56c2c33d6b44bf575223f3c34cb
Joshua Bugryn <46905032+bugryn-josh@users.noreply.github.com>
2019-10-08 21:53:48 -0400

    fixed columns and output

--- network.sh
+++ network.sh
@@ -1,2 +1,6 @@
-ifstat | awk '$1 == "ens33" {print $6","$8}'
-ifstat | awk '$1 == "ens33" {print $6","$8}'
+ifstat | awk '$1 == "ens33" {print substr($7, 0, length($7) - 1)","substr($9, 0, length($9) - 1}'
+
+
+start ifstat with parameters to use 100 second intervals
+
+CLOSE IFSTAT IN EXIT TRAP -- Keep track of IFSTAT PID
